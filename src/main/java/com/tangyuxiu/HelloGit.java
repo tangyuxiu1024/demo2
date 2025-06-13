f483da429f638804cb4b7857e812c1f20997b930
tangyuxiu <1556512912@qq.com>
2025-06-13 14:47:51 +0800

    hot-fix 修改1

--- src/main/java/com/tangyuxiu/HelloGit.java
+++ src/main/java/com/tangyuxiu/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("第二次修改");
         System.out.println("第三次修改");
+        System.out.println("hot-fix");
     }
 }
